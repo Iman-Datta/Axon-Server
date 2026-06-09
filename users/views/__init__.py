dd9958206446c51ce4f1388093b03e8ae07d41d4
Iman Datta <dattaiman56@gmail.com>
2026-06-10 01:12:33 +0530

    Moduler struct

---
+++ users/views/__init__.py
@@ -0,0 +1,5 @@
+from .auth_views import *
+
+from .oauth_views import *
+
+from .profile_views import *
--- users/views/auth_views.py
+++ users/views/auth_views.py
@@ -1,6 +1,4 @@
 import hashlib
-import requests
-import secrets
 
 from rest_framework.decorators import (api_view, permission_classes)
 from rest_framework.permissions import IsAuthenticated
@@ -13,14 +11,13 @@ from django.shortcuts import redirect
 from django.contrib.auth import authenticate
 from urllib.parse import urlencode
 
-from .serializers import RegisterSerializer, LoginSerializer
+from ..serializers import RegisterSerializer, LoginSerializer
 
 from django.utils import timezone
 
 from django.conf import settings
-from .models import User
-from .utils.email_verification import send_verification_email
-
+from ..models import User
+from ..utils.email_verification import send_verification_email
 
 @api_view(['POST'])
 def register_view(request):
@@ -360,168 +357,3 @@ def logout_view(request):
     response.delete_cookie("refresh_token")
     return response
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def me_view(request):
-
-    try:
-        user = request.user
-        if not user:
-            return Response({"message": "User not found","success": False}, status=404)
-        
-        avatar_url = None
-
-        if user.avatar:
-            avatar_url = request.build_absolute_uri(
-                user.avatar.url
-            )
-
-        user_data = {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
-
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-
-            "avatar": avatar_url,
-            "bio": user.bio,
-
-            "is_email_verified": user.is_email_verified,
-            "is_profile_completed": user.is_profile_completed,
-
-            "github_profile": user.github_profile,
-            "linkedin_profile": user.linkedin_profile,
-            "portfolio_website": user.portfolio_website,
-
-            "created_at": user.created_at,
-            "updated_at": user.updated_at,
-        }
-
-        return Response({"user": user_data,"success": True}, status=200)
-    
-    except Exception as e:
-        return Response(
-            {"message": str(e),"success": False},status=500)
-
-@api_view(['GET'])
-def google_login_view():
-    google_url = (
-        "https://accounts.google.com/o/oauth2/v2/auth?"
-        +
-        urlencode(
-            {
-                "client_id": settings.GOOGLE_CLIENT_ID,
-                "redirect_uri": settings.GOOGLE_REDIRECT_URI,
-                "response_type": "code",
-                "scope": "openid email profile",
-                "access_type": "offline",
-                "prompt": "select_account",
-            }
-        )
-    )
-    return redirect(google_url)
-
-@api_view(["GET"])
-def google_callback_view(request):
-    code = request.GET.get("code")
-    if not code:
-        return redirect(f"{settings.FRONTEND_URL}/auth")
-
-    try:
-        token_response = requests.post(
-            "https://oauth2.googleapis.com/token",
-
-            data={
-                "code": code,
-                "client_id": settings.GOOGLE_CLIENT_ID,
-                "client_secret": settings.GOOGLE_CLIENT_SECRET,
-                "redirect_uri": settings.GOOGLE_REDIRECT_URI,
-                "grant_type": "authorization_code",
-            },
-
-            timeout=10
-        )
-        if token_response.status_code != 200:
-            return redirect(f"{settings.FRONTEND_URL}/auth")
-
-        token_data = token_response.json()
-        google_access_token = token_data.get("access_token")
-
-        if not google_access_token:
-           return redirect(f"{settings.FRONTEND_URL}/auth")
-
-        user_response = requests.get(
-            "https://www.googleapis.com/oauth2/v2/userinfo",
-
-            headers={
-                "Authorization": (f"Bearer {google_access_token}")
-            },
-            timeout=10
-        )
-        if user_response.status_code != 200:
-            return redirect(f"{settings.FRONTEND_URL}/auth")
-        
-        google_user = user_response.json()
-
-        if not google_user.get("verified_email"):
-            return redirect(f"{settings.FRONTEND_URL}/auth")
-
-        
-        email = google_user.get("email")
-        first_name = google_user.get("given_name", "")
-        last_name = google_user.get("family_name", "")
-
-        user = User.objects.filter( email=email).first()
-
-        if user:
-            user.is_email_verified = True
-            user.save(
-                update_fields=[
-                    "is_email_verified"
-                ]
-            )
-
-        else: # New Google account:
-
-            temp_username = f"user_{secrets.token_hex(8)}"
-
-            user = User.objects.create(
-                username=temp_username,
-                email=email,
-                first_name=first_name,
-                last_name=last_name,
-                is_email_verified=True,
-                is_profile_completed=False,
-            )
-
-        refresh = RefreshToken.for_user(user)
-        refresh_token = str(refresh)
-
-        hashed_refresh = hashlib.sha256(refresh_token.encode()).hexdigest()
-
-        user.refresh_token_hash = hashed_refresh
-
-        user.save(
-            update_fields=[
-                "refresh_token_hash"
-            ]
-        )
-
-        response = redirect(f"{settings.FRONTEND_URL}/dashboard")
-
-        response.set_cookie(
-            key="refresh_token",
-            value=refresh_token,
-            httponly=True,
-            secure=False,   # True in production HTTPS
-            samesite="Lax",
-            max_age=7 * 24 * 60 * 60,
-
-        )
-        return response
-
-    except Exception as error:
-        print(error)
-        return redirect(f"{settings.FRONTEND_URL}/auth")
-    
