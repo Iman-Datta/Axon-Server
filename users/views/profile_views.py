dea107261391a62c473fa580d1b92c022568580c
Iman Datta <dattaiman56@gmail.com>
2026-06-13 00:05:10 +0530

    check username

--- users/views/profile_views.py
+++ users/views/profile_views.py
@@ -2,39 +2,43 @@ from rest_framework.decorators import (api_view, permission_classes)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from ..models import User
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def me_view(request):
+    user = request.user
 
-    try:
-        user = request.user
-        
-        user_data = {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
+    user_data = {
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
 
-            "first_name": user.first_name,
-            "last_name": user.last_name,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
 
-            "avatar": user.avatar,
-            "bio": user.bio,
+        "avatar": user.avatar,
+        "bio": user.bio,
 
-            "is_username_set": user.is_username_set,
-            "is_email_verified": user.is_email_verified,
-            "is_profile_completed": user.is_profile_completed,
+        "is_username_set": user.is_username_set,
+        "is_email_verified": user.is_email_verified,
+        "is_profile_completed": user.is_profile_completed,
 
-            "github_profile": user.github_profile,
-            "linkedin_profile": user.linkedin_profile,
-            "portfolio_website": user.portfolio_website,
+        "github_profile": user.github_profile,
+        "linkedin_profile": user.linkedin_profile,
+        "portfolio_website": user.portfolio_website,
 
-            "created_at": user.created_at,
-            "updated_at": user.updated_at,
-        }
-        return Response({"user": user_data,"success": True}, status=200)
-    except Exception as e:
-        return Response(
-            {"message": str(e),"success": False},status=500)
+        "created_at": user.created_at,
+        "updated_at": user.updated_at,
+    }
+
+    return Response(
+        {
+            "user": user_data,
+            "success": True
+        },
+        status=200
+    )
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
@@ -65,3 +69,37 @@ def onboarding_status_view(request):
         },
         status=200
     )
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def check_username_view(request):
+    username = request.GET.get("username")
+
+
+    if not username:
+        return Response({
+                "success": False,
+                "message": "Username is required."
+            },
+            status=400
+        )
+    
+    username = username.strip()
+
+    if(len(username) < 4):
+         return Response({            
+                "success": False,
+                "message": "Username must contain at least 4 characters."
+            },
+            status=400
+        )
+    
+    username_exists = User.objects.filter(username__iexact=username).exists()
+
+    return Response({
+            "success": True,
+            "available": not username_exists
+        },
+        status=200
+    )
+
