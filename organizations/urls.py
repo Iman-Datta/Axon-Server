55348bc9e8b1c020a37f994d5f5200d920f6df85
Iman Datta <dattaiman56@gmail.com>
2026-06-16 12:36:22 +0530

    Implement organization CRUD with owner membership system

--- organizations/urls.py
+++ organizations/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from view.organization import (create_org, my_org, update_org,delete_org,)
 
+urlpatterns = [
+    path("/create", create_org, name="create-org"),
+    path("my/", my_org, name="my-org"),
+    path("<slug:slug>/", update_org, name="update-org"),
+    path("<slug:slug>/delete/", delete_org, name="delete-org"),
 ]
---
+++ organizations/view/organization.py
@@ -0,0 +1,123 @@
+from rest_framework.decorators import (api_view, permission_classes)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from ..serializers import OrganizationSerializer
+from ..models import Organization, OrganizationMember
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def create_org(request):
+    serializer = OrganizationSerializer(
+        data = request.data,
+        context = {
+            "user" :  request.user
+        }
+    )
+
+    if not serializer.is_valid():
+        return Response({
+            "success": False,
+            "errors": serializer.errors
+        },status=400)
+    
+    organization = serializer.save()
+    
+    return Response({
+        "success": True,
+        "message": "Organization created successfully.",
+        "organization": OrganizationSerializer(organization).data
+    }, status=201)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def my_org(request):
+    organizations = Organization.objects.filter(members__user = request.user) # Find Organizations whose members contain this user.
+
+    serializer = OrganizationSerializer(
+        organizations,
+        many = True,
+    )
+
+    return Response({
+            "success": True,
+            "organizations": serializer.data
+        },
+        status=200
+    )
+
+@api_view(["PATCH"])
+@permission_classes([IsAuthenticated])
+def update_org(request, slug):
+    try:
+        organization = Organization.objects.get(
+            slug = slug,
+            members__user=request.user
+        )
+    except Organization.DoesNotExist:
+        return Response({
+            "success": False,
+            "message": "Organization not found."
+        }, status=400)
+    
+    serializer = OrganizationSerializer(
+        organization,
+        data = request.data,
+        partial = True
+    )
+
+    if not serializer.is_valid():
+        return Response ({
+            "success": False,
+            "errors": serializer.errors
+        }, status=400)
+    
+    serializer.save()
+    return Response({
+            "success": True,
+            "message": "Organization updated successfully.",
+            "organization": serializer.data
+        },
+        status=200
+    )
+
+@api_view(["DELETE"])
+@permission_classes([IsAuthenticated])
+def delete_org(request, slug):
+    try:
+        organization = Organization.objects.get(
+            slug=slug
+        )
+
+    except Organization.DoesNotExist:
+        return Response(
+            {
+                "success": False,
+                "message": "Organization not found."
+            },
+            status=404
+        )
+
+    is_owner = OrganizationMember.objects.filter(
+        organization=organization,
+        user=request.user,
+        role=OrganizationMember.Role.OWNER
+    ).exists()
+
+    if not is_owner:
+        return Response(
+            {
+                "success": False,
+                "message": "Only owner can delete organization."
+            },
+            status=403
+        )
+    
+    organization.delete()
+
+    return Response(
+        {
+            "success": True,
+            "message": "Organization deleted successfully."
+        },
+        status=200
+    )
