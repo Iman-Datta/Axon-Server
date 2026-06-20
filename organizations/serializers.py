ddb0668c1a83cf51078f5128a9e21bc150433d11
Iman Datta <dattaiman56@gmail.com>
2026-06-20 10:41:40 +0530

    feat: refactor organization management views and serializers; remove unused files

--- organizations/serializers.py
+++ organizations/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Organization, OrganizationMember
-from ..users.models import User
+from users.models import User
 
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
