e564c1d113eb64557c5e2df5e81a117a61ec8454
Iman Datta <dattaiman56@gmail.com>
2026-06-26 17:29:12 +0530

    Project model complete

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from organizations.models import Organization
 
-# Create your models here.
+class Project(models.Model):
+    VISIBILITY_CHOICES = [
+        ("public", "Public"),
+        ("private", "Private"),
+    ]
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max=100)
