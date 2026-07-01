d61b26b810ad59e3205baadb15549abc21e87b4a
Iman Datta <dattaiman56@gmail.com>
2026-07-01 12:48:50 +0530

    Project view and Serializer and also Decoretor for resolve workspace

---
+++ projects/decorators.py
@@ -0,0 +1,35 @@
+from functools import wraps
+
+from rest_framework.response import Response
+from rest_framework import status
+
+from users.models import User
+from organizations.models import Organization
+
+def resolve_workspace(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        slug = kwargs.get("slug")
+        if not slug:
+            return Response({"message": "Workspace slug is required."},status=status.HTTP_400_BAD_REQUEST,)
+                
+        workspace = None
+
+        try:
+            user = User.objects.select_related("workspace").get(username = slug)
+            workspace = user.workspace
+        except User.DoesNotExist:
+            try:
+                organization = Organization.objects.select_related("workspace").get(slug=slug)
+                workspace = organization.workspace
+            except Organization.DoesNotExist:
+                return Response(
+                    {
+                        "message": "Workspace not found.",
+                        "success": False,
+                    },
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+        request.workspace = workspace
+        return view_func(request, *args, **kwargs)
+    return wrapper
--- projects/views/project.py
+++ projects/views/project.py
@@ -1,16 +1,51 @@
 from rest_framework.decorators import (api_view, permission_classes)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.db import transaction
 
 from ..serializers.project import ProjectCreateSerializer
+from ..decorators import resolve_workspace
+from ..models import Project, ProjectMember
+from organizations.permissions import has_admin_permission
+
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
-def project_personal_create_view(request):
-    user = request.user
-    serializer = ProjectCreateSerializer(
-        data = request.data,
-        context = {
-            "user" :  user,
-        }
-    )
+@resolve_workspace
+def create_project_view(request, slug):
+    serializer = ProjectCreateSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    workspace = request.workspace
+
+    # Personal Workspace Permission
+    if workspace.type == "personal":
+        if workspace.owner != request.user:
+            return Response({"message": "Permission denied."},status=403)
+
+    # Organization Workspace Permission
+    else:
+        if not has_admin_permission(request.user,workspace.organization):
+            return Response({"message": "Only organization admins can create projects."},status=403)
+        
+
+    with transaction.atomic():    
+        project = Project.objects.create(
+            workspace=workspace,
+            created_by=request.user,
+            **serializer.validated_data
+        )
+
+        ProjectMember.objects.create(
+            project=project,
+            user=request.user,
+            role=ProjectMember.Role.OWNER
+        )
+    return Response({
+            "message": "Project created successfully.",
+            "project": {
+                "id": project.id,
+                "name": project.name,
+                "slug": project.slug,
+            }
+        },status=201)
