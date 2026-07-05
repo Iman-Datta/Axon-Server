5bcc0b8cc9d9aed8823c3ac6fceb381f5142aea1
Iman Datta <dattaiman56@gmail.com>
2026-07-06 00:52:52 +0530

    Migrations

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,25 +1,20 @@
 from django.urls import path
 
-from .views.epic import (create_epic,update_epic,list_epics,delete_epic)
-from .views.ticket import (
-    create_ticket,
-    update_ticket,
-    list_tickets,
-    retrieve_ticket,
-    delete_ticket,
-    assign_ticket,
-)
+from .views.epic import create_epic, update_epic, list_epics, delete_epic
+from .views.ticket import (create_ticket,update_ticket,list_tickets,retrieve_ticket,delete_ticket,assign_ticket)
 
-rlpatterns = [
-    path("", list_epics, name="list_epics"),
-    path("", create_epic, name="create_epic"),          # POST
-    path("<int:epic_id>/", update_epic, name="update_epic"),   # PATCH
-    path("<int:epic_id>/", delete_epic, name="delete_epic"),   # DELETE
+urlpatterns = [
+    # Epic
+    path("epics/", list_epics, name="list_epics"),
+    path("epics/create/", create_epic, name="create_epic"),
+    path("epics/<int:epic_id>/update/", update_epic, name="update_epic"),
+    path("epics/<int:epic_id>/delete/", delete_epic, name="delete_epic"),
 
-    path("", list_tickets, name="list_tickets"),          # GET
-    path("create/", create_ticket, name="create_ticket"), # POST
-    path("<int:ticket_id>/", retrieve_ticket, name="retrieve_ticket"),  # GET
-    path("<int:ticket_id>/update/", update_ticket, name="update_ticket"),# PATCH
-    path("<int:ticket_id>/delete/", delete_ticket, name="delete_ticket"),# DELETE
-    path("<int:ticket_id>/assign/", assign_ticket, name="assign_ticket"),# PATCH
+    # Ticket
+    path("", list_tickets, name="list_tickets"),
+    path("create/", create_ticket, name="create_ticket"),
+    path("<int:ticket_id>/", retrieve_ticket, name="retrieve_ticket"),
+    path("<int:ticket_id>/update/", update_ticket, name="update_ticket"),
+    path("<int:ticket_id>/delete/", delete_ticket, name="delete_ticket"),
+    path("<int:ticket_id>/assign/", assign_ticket, name="assign_ticket"),
 ]
