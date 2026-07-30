4c465a8bae07d2aacdda6df2d0d731cb90920c25
Iman Datta <dattaiman56@gmail.com>
2026-07-30 22:33:23 +0530

    Epic details API with Epic id

--- tickets/serializers/epic.py
+++ tickets/serializers/epic.py
@@ -2,7 +2,7 @@ import re
 
 from rest_framework import serializers
 
-from ..models import Epic
+from ..models import Epic, Ticket
 from users.models import User
 from projects.models import ProjectMember
 
@@ -34,9 +34,11 @@ class UserMiniSerializer(serializers.ModelSerializer):
 
         return membership.role if membership else None
 
-
 class EpicSerializer(serializers.ModelSerializer):
     created_by = serializers.SerializerMethodField()
+    ticket_count = serializers.IntegerField(read_only=True)
+    completed_count = serializers.IntegerField(read_only=True)
+    progress = serializers.SerializerMethodField()
 
     class Meta:
         model = Epic
@@ -45,6 +47,11 @@ class EpicSerializer(serializers.ModelSerializer):
             "name",
             "description",
             "color",
+
+            "ticket_count",
+            "completed_count",
+            "progress",
+
             "created_by",
             "created_at",
             "updated_at",
@@ -65,6 +72,11 @@ class EpicSerializer(serializers.ModelSerializer):
             obj.created_by,
             context={"project": obj.project},
         ).data
+
+    def get_progress(self, obj):
+        if obj.ticket_count == 0:
+            return 0
+        return round(obj.completed_count * 100 / obj.ticket_count)
 
     def validate_color(self, value):
         if not re.fullmatch(r"^#[0-9A-Fa-f]{6}$", value):
@@ -115,3 +127,68 @@ class EpicSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
+
+class TicketMiniSerializer(serializers.ModelSerializer):
+    assignee = UserMiniSerializer(read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = [
+            "id",
+            "ticket_number",
+            "title",
+            "kanban_column",
+            "priority",
+            "assignee",
+        ]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+
+        if data["assignee"] is not None:
+            data["assignee"] = UserMiniSerializer(
+                instance.assignee,
+                context=self.context,
+            ).data
+        return data
+
+class EpicDetailsSerializer(serializers.ModelSerializer):
+    created_by = serializers.SerializerMethodField()
+
+    ticket_count = serializers.IntegerField(read_only=True)
+    completed_count = serializers.IntegerField(read_only=True)
+    progress = serializers.SerializerMethodField()
+
+    tickets = TicketMiniSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Epic
+        fields = [
+            "id",
+            "name",
+            "description",
+            "color",
+
+            "ticket_count",
+            "completed_count",
+            "progress",
+
+            "created_by",
+
+            "created_at",
+            "updated_at",
+
+            "tickets",
+        ]
+
+    def get_created_by(self, obj):
+        return UserMiniSerializer(obj.created_by,context=self.context).data if obj.created_by else None
+
+    def get_progress(self, obj):
+        if obj.ticket_count == 0:
+            return 0
+
+        return round(obj.completed_count * 100 / obj.ticket_count)
