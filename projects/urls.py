76727197493acbf425eede190767b1166cd2bae9
Iman Datta <dattaiman56@gmail.com>
2026-07-30 16:53:50 +0530

    Debug my_projects_view API send the owner workspace to react

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views.project import (create_project_view,list_projects_view,project_detail_view,project_update_view,delete_project)
+from .views.project import (create_project_view,project_detail_view,project_update_view,delete_project, my_projects_view)
 
 from .views.member import (add_member,list_member,update_member_role,remove_member,leave_project)
 
@@ -10,7 +10,7 @@ from .views.webhook import (github_webhook_view, create_github_webhook_view)
 
 urlpatterns = [
     # Projects
-    path("<slug:slug>/", list_projects_view),
+    path("<slug:slug>/my/",my_projects_view,name="workspace-projects"),
     path("<slug:slug>/create/", create_project_view),
     path("workspaces/<slug:slug>/projects/<slug:project_slug>/", project_detail_view),
     path("workspaces/<slug:slug>/projects/<slug:project_slug>/update/", project_update_view),
@@ -18,7 +18,7 @@ urlpatterns = [
 
     # Members
     path("<slug:slug>/<slug:project_slug>/members/", list_member),
-    path("workspaces/<slug:slug>/projects/<slug:project_slug>/members/add/", add_member),
+    path("<slug:slug>/<slug:project_slug>/member/add/", add_member),
     path("workspaces/<slug:slug>/projects/<slug:project_slug>/members/<int:member_id>/role/", update_member_role),
     path("workspaces/<slug:slug>/projects/<slug:project_slug>/members/<int:member_id>/remove/", remove_member),
     path("workspaces/<slug:slug>/projects/<slug:project_slug>/leave/", leave_project),
