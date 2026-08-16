c9c4326bec955d86beb40fe85ffe7a1dfff69720
Iman Datta <dattaiman56@gmail.com>
2026-08-16 14:35:34 +0530

    feat: add avatar field to Organization model and implement avatar upload functionality

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import register_view, verify_magiclink_view, refresh_token_view, login_view, logout_view,me_view, google_login_view, google_callback_view, github_login_view, github_callback_view, check_username_view, update_username_view, send_otp_view, verify_email_otp_view, github_connect_view, complete_profile_view, public_profile_view, workspace_detail_view, update_profile_view, update_profile_password_view, my_overview, my_work_tickets, update_profile_avatar
+from .views import register_view, verify_magiclink_view, refresh_token_view, login_view, logout_view,me_view, google_login_view, google_callback_view, github_login_view, github_callback_view, check_username_view, update_username_view, send_otp_view, verify_email_otp_view, github_connect_view, complete_profile_view, public_profile_view, workspace_detail_view, update_profile_view, update_profile_password_view, my_overview, my_work_tickets, update_workspace_avatar
 
 urlpatterns = [
     # Core auth
@@ -31,10 +31,10 @@ urlpatterns = [
     path("profile/update", update_profile_view, name="update-profile"),
     path("profile/password", update_profile_password_view, name="update-password"),
     path("my/overview/", my_overview, name="my_overview"),
-    path("profile/avtar/update/", update_profile_avatar),
     path("my/work/", my_work_tickets, name="my-work-tickets"),
     path("<str:username>/",public_profile_view,name="public-profile"),
 
     # Workspace
     path("workspaces/<slug:slug>/", workspace_detail_view),
+    path("<slug:slug>/avtar/update/", update_workspace_avatar),
 ]
