99066ec0523ac58400b311667790932c66b108a9
Iman Datta <dattaiman56@gmail.com>
2026-08-23 10:03:11 +0530

    update url

--- activity/urls.py
+++ activity/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ticket_activity_list, project_activity_list
 
 urlpatterns = [
-    path("project/<int:ticket_id>/", ticket_activity_list, name="get-ticket-activities"),
+    path("ticket/<int:ticket_id>/", ticket_activity_list, name="get-ticket-activities"),
     path("project/", project_activity_list, name="get-project-activities"),
 ]
